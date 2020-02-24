d825b7cccbe8d21154480594db511d2c534e901b
Sankara Ekane <sankaraekane@Sankaras-MacBook-Pro.local>
2020-02-24 09:56:37 -0500

    Created TestCreateMatch test: This test checks if createMatchHandler perfrom correctly

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/Sankara98/gogo-service
+
+go 1.13
+
+require (
+	github.com/codegangsta/negroni v1.0.0
+	github.com/gorilla/mux v1.7.4
+	github.com/unrolled/render v1.0.2
+)
---
+++ handlers_test.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/unrolled/render"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var (
+	formatter = render.New(render.Options{
+		IndentJSON: true,
+	})
+)
+
+func TestCreateMatch(t *testing.T) {
+	client := &http.Client{}
+	server := httptest.NewServer(
+		http.Handler(createMatchHandler(formatter)))
+	defer server.Close()
+
+	body := []byte("{\n\"gridsize\": 19,\n \"players\": [\n	{\n\"color\": \"white\",\n		\"name\": \"bob\"\n		},\n	{\n\"color\": \"black\", \n	\"name\": \"alfred\"\n	},\n	} ]	\n}")
+
+	req, err := http.NewRequest("POST",
+		server.URL, bytes.NewBuffer(body))
+	if err != nil {
+		t.Errorf("Error in creating post request for createMatchHandle: %v", err)
+	}
+
+	req.Header.Add("Content", "application/json")
+
+	res, err := client.Do(req)
+	if err != nil {
+		t.Errorf("Error in POST to createMatchHandler %v", err)
+	}
+	defer res.Body.Close()
+
+	payload, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		t.Errorf("Error reading response body: %v", err)
+	}
+	if res.StatusCode != http.StatusCreated {
+		t.Errorf("Expected response status 201, received %s", res.Status)
+	}
+
+	fmt.Printf("Payload: %s", string(payload))
+
+}
