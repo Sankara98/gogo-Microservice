e75ea136ab5fc9a76fd90f41a841431f7060a6bd
Sankara Ekane <sankaraekane@Sankaras-MacBook-Pro.local>
2020-02-24 22:36:20 -0500

    CreateMatchHandler modified to fit tests

--- handlers.go
+++ handlers.go
@@ -1,13 +1,39 @@
 package main
 
 import (
+	"encoding/json"
+	"github.com/cloudnativego/gogo-engine"
 	"github.com/unrolled/render"
+	"io/ioutil"
 	"net/http"
 )
 
-func createMatchHandler(formatter *render.Render) http.HandlerFunc {
+func createMatchHandler(formatter *render.Render, matchRepository matchRepository) http.HandlerFunc {
 	return func(w http.ResponseWriter, req *http.Request) {
-		formatter.JSON(w, http.StatusOK, struct{ Test string }{"This is a test"})
+		payload, _ := ioutil.ReadAll(req.Body)
+		var newMatchRequest newMatchRequest
+		err := json.Unmarshal(payload, &newMatchRequest)
+		if err != nil {
+			formatter.Text(w, http.StatusBadRequest,
+				"Failed to parse create match request")
+			return
+		}
+		if !newMatchRequest.isValid() {
+			formatter.Text(w, http.StatusBadRequest,
+				"Invalid new match request")
+			return
+		}
+
+		newMatch := gogo.NewMatch(newMatchRequest.GridSize,
+			newMatchRequest.PlayerBlack, newMatchRequest.PlayerWhite)
+		//matchRepository.addMatch(newMatch)
+		w.Header().Add("Location", "/matches/"+newMatch.ID)
+		formatter.JSON(w, http.StatusCreated,
+			&newMatchResponse{
+				ID:          newMatch.ID,
+				GridSize:    newMatch.GridSize,
+				PlayerWhite: newMatch.PlayerWhite,
+				PlayerBlack: newMatch.PlayerBlack})
 	}
 
 }
